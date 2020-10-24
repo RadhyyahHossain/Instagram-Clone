2c8f880a0ceb1cf443657e974ec16a183881c96c
Radhyyah Hossain <radiabxd@gmail.com>
2020-10-23 21:10:44 -0400

    Created the home page that shows the image and caption posted with username.

---
+++ Instagram/PostCell.swift
@@ -0,0 +1,31 @@
+//
+//  PostCell.swift
+//  Instagram
+//
+//  Created by Radhyyah Hossain on 10/23/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
