7fed5a29da53d2b8597d2755721ea3eaf74fb75b
Radhyyah Hossain <radiabxd@gmail.com>
2020-10-23 20:09:49 -0400

    Made it so when you click on post after choosing a photo, it will save to parse and self dismiss it. Next is making the home page.

--- Instagram/CameraViewController.swift
+++ Instagram/CameraViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AlamofireImage
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -24,6 +25,26 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     
 
     @IBAction func onSubmitButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = commentField.text
+        post["author"] = PFUser.current()!
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        
+        post["image"] = file
+        
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("saved!")
+            } else {
+                print("error!")
+            }
+        }
+        
     }
     
     
