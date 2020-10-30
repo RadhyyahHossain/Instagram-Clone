ff0452eb7c6291bde1be5a1f865e79eac70a5707
Radhyyah Hossain <radiabxd@gmail.com>
2020-10-30 18:49:27 -0400

    going to make fake comment

---
+++ Instagram/CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  Instagram
+//
+//  Created by Radhyyah Hossain on 10/30/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
