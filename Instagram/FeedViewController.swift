d67489864d44b88c7971c1a217d151ef3c77efb6
Radhyyah Hossain <radiabxd@gmail.com>
2020-10-30 18:20:00 -0400

    Created a fake comment when tapped on a post. Will show the comment in the post next.

--- Instagram/FeedViewController.swift
+++ Instagram/FeedViewController.swift
@@ -70,6 +70,31 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = posts[indexPath.row]
+        
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "This is a rando comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
+        
+        post.add(comment, forKey: "comments")
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                print("Comment saved!")
+            } else {
+                print("Error saving comment")
+            }
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
     /*
     // MARK: - Navigation
 
